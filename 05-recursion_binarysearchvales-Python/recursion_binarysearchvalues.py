0f89d93c43723878d95a4a969e7b1838c6e9d8c7
Vijayabhanuuu <vijayabhanu34@msitprogram.net>
2021-08-31 08:36:33 +0530

    First Commit

--- 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
+++ 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
@@ -16,8 +16,40 @@
 #     v = 'c'
 #     assert(binarySearchValues(L, v) == [(2,'f'), (0,'a'), (1,'c')])
 # Hint: Do not slice the list L, but rather adjust the indexes into L. 
+def binary_search2(arr,low,l,x,res):
+	
+	if l >= low:
+
+		m = (l + low) // 2
+		print('midval',m,'lowval',low,'highval',l)
+		if arr[m] == x:
+			temp = (m, arr[m])
+			print(temp)
+			res.append(temp)
+			return res
+			
+
+		elif arr[m] > x:
+			temp = (m, arr[m])
+			print(temp)
+			res.append(temp)
+			return binary_search2(arr, low, m - 1, x, res) 
+		else:
+			temp = (m, arr[m])
+			print(temp)
+			res.append(temp)
+			return binary_search2(arr, m + 1, l, x, res) 
+	else:
+		return res
+
 
 def recursion_binarysearchvalues(L, v):
 	# Your codes goes here
-	pass
+	res =[]
+	# Your codes goes here
+	arr = L
+	low = 0
+	l = len(L)-1
+	x = v
+	return binary_search2(arr,low,l,x,res)
 	
